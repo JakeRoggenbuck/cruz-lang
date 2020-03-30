ed8383eea5ae6744ca9dac1f4b78149a5fa67df2
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-03-30 13:01:58 -0700

    Add lexer to cshark

--- shark/cshark.py
+++ shark/cshark.py
@@ -1,6 +1,14 @@
-class Parse():
+import sharlex
 
-    def read(self, fileName):
-        with open(fileName) as f:
-            content = f.readlines()
-            return content
+def read(fileName):
+    with open(fileName) as f:
+        lines = []
+        lines += f.readlines()
+        return lines
+
+file_ = read('lex_test_file.cz')
+
+data = """{}""".format("".join(file_[0:]))
+
+sharlex.parlex(data)
+
