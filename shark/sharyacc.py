51e2921466c901d19baf415d78e53dfbb632007d
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-04-01 12:22:43 -0700

    Add start basic yacc

---
+++ shark/sharyacc.py
@@ -0,0 +1,26 @@
+import ply.yacc as yacc
+
+def p_binary_operators(p):
+    '''expression : expression PLUS term
+                  | expression MINUS term'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+   try:
+       s = raw_input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
